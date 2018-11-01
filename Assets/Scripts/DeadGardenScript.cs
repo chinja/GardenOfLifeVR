8dc19193ed5b7bb27e362a9d7b0b54acf65269fd
Lee <35541778+Harrolee@users.noreply.github.com>
2018-11-01 10:27:29 -0700

    Initial Commit - Working scene change

---
+++ Assets/Scripts/DeadGardenScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeadGardenScript : MonoBehaviour {
+
+    private AssetBundle myLoadedAssetBundle;
+    private string[] scenePaths;
+    public bool entered_tomb = false;
+
+    // Use this for initialization
+    /*
+    void Start () {
+        myLoadedAssetBundle = AssetBundle.LoadFromFile("Assets/AssetBundles/scenes");
+        scenePaths = myLoadedAssetBundle.GetAllScenePaths();
+    }
+    */
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log(col.name);
+        if (col.name == "tomb_stone")
+        {
+            ChangeScene();
+        }
+    }
+
+    void ChangeScene()
+    {
+        Debug.Log("Loading Alive Garden");
+        SceneManager.LoadScene("Scenes/alive_garden", LoadSceneMode.Single);
+    }
+}
