8e844b3198bd3b05d3732a1330f83f375573c7f0
Dobby <>
2018-11-08 09:58:33 -0800

    Lighting/Environment Setup
    Added skyboxes. Also broken title scene.

---
+++ Assets/Scripts/LoadScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScene : MonoBehaviour {
+
+	public void ChangeScene()
+    {
+        // Changes to Game Scene.
+        Debug.Log("Change Scene.");
+        SceneManager.LoadScene("dead_garden");
+    }
+}
